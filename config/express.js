0e3b583c448bd1b27d5562edab1da25117c6c115
Isabela Porto <isamporto@hotmail.com>
2023-11-28 22:25:47 -0300

    Feat: create router file in order to not polute express file

--- config/express.js
+++ config/express.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const router = require("../router");
 
 module.exports = () => {
 	const app = express();
 
 	app.set("port", 3000);
+	app.use(router);
 
 	return app;
 };
